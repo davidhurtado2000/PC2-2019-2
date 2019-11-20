ea3acd016f8fbaf8ab921281c031ee3b7d2b3d84
David <david.huetado2000@gmail.com>
2019-11-20 08:39:26 -0500

    commit 3

---
+++ pregunta2.py
@@ -0,0 +1,16 @@
+texto1 = []
+letras1 = []
+contado = [[]]
+
+def contarletra(texto, letra):
+    texto1.append(texto)
+    letras1.append(letra)
+    if texto1[0] == letras1[0]:
+        contado.append[letra]
+        return contado.count[i]
+    
+
+A = contarletra("hola mundo", "o")
+B = contarletra("1231133421", "1")
+print(contado)
+
